78011378fc0ebb8b698a107f61456581fc228766
leejinseok <sonaky47@gmail.com>
2023-09-12 16:12:12 +0900

    soundPool 설정

--- app/src/main/java/com/example/xylophone/MainActivity.kt
+++ app/src/main/java/com/example/xylophone/MainActivity.kt
@@ -1,46 +1,39 @@
 package com.example.xylophone
 
+import android.media.SoundPool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.xylophone.ui.theme.XylophoneTheme
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val soundPool = SoundPool.Builder().setMaxStreams(8).build()
+        val sounds = listOf(
+            SoundData(soundPool.load(this, R.raw.do1, 1), "도", Color.Red),
+            SoundData(soundPool.load(this, R.raw.re, 1), "레", Color.Black),
+            SoundData(soundPool.load(this, R.raw.mi, 1), "미", Color.Black),
+            SoundData(soundPool.load(this, R.raw.fa, 1), "파", Color.Black),
+            SoundData(soundPool.load(this, R.raw.sol, 1), "솔", Color.Black),
+            SoundData(soundPool.load(this, R.raw.la, 1), "라", Color.Black),
+            SoundData(soundPool.load(this, R.raw.si, 1), "시", Color.Black),
+            SoundData(soundPool.load(this, R.raw.do2, 1), "도", Color.Red),
+        )
+
+        fun playSound(index: Int) {
+            soundPool.play(sounds[index].soundId, 1f, 1f, 0, 0,  1f)
+        }
+
         setContent {
-            XylophoneTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun XylophoneScreen() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    XylophoneTheme {
-        Greeting("Android")
-    }
 }
---
+++ app/src/main/java/com/example/xylophone/SoundData.kt
@@ -0,0 +1,9 @@
+package com.example.xylophone
+
+import androidx.compose.ui.graphics.Color
+
+data class SoundData(
+    val soundId: Int,
+    val text: String,
+    val color: Color
+)
